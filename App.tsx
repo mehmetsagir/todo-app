53ece953beecf0a05ae2e4e127f253fdcdcdf578
mehmetsagir <memetsagirint@gmail.com>
2022-03-05 16:27:06 +0300

    add StatusBar

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import 'react-native-gesture-handler';
 
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 
 import { TodosContextProvider } from './src/hooks/useTodos';
@@ -8,6 +9,7 @@ import AppNavigation from './src/navigation/AppNavigation';
 export default function App({ children }: { children: React.ReactNode }) {
   return (
     <TodosContextProvider>
+      <StatusBar style="light" />
       <AppNavigation />
       {children}
     </TodosContextProvider>
